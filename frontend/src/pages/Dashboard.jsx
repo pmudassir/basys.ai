acf4b15ba3684799da46c73f4c209fc1f84969ff
pmudassir <pmuddasir@gmail.com>
2024-10-18 07:24:01 +0530

    minor chnage

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -34,9 +34,6 @@ const Dashboard = () => {
               Here&apos;s a list of Patients.
             </p>
           </div>
-          <div className="flex items-center space-x-2">
-            Hello
-          </div>
         </div>
         <DataTable columns={getColumns(navigate)} data={data} refreshPatient={getPatients} />
       </div>
